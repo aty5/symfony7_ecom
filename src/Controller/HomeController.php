04e596214ebf1e652f2f9c7fca3887e025cd5405
aty5 <atybuild@hotmail.com>
2025-02-08 19:59:29 +0100

    edit header by admin functionality

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 
 use App\Class\Mail;
+use App\Repository\HeaderRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,8 +12,11 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(HeaderRepository $headerRepository): Response
     {
-        return $this->render('home/index.html.twig', []);
+
+        return $this->render('home/index.html.twig', [
+            'headers' => $headerRepository->findAll(),
+        ]);
     }
 }
