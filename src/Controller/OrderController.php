d9e7584400352219d8acb4ad2af699b445754737
aty5 <atybuild@hotmail.com>
2025-02-08 19:59:27 +0100

    /order/summary

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Class\Cart;
 use App\Form\OrderType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -25,10 +27,45 @@ class OrderController extends AbstractController
 
         $form = $this->createForm(OrderType::class, null, [
             'addresses' => $addresses,
+            'action' => $this->generateUrl('app_order_summary')
         ]);
+
 
         return $this->render('order/index.html.twig', [
             'deliveryForm' => $form->createView(),
+        ]);
+    }
+
+    /*
+     * 2eme etape du tunnel d achat
+     * recap commande
+     * insertion en bdd
+     * preparation paiement STRIPE
+     */
+    #[Route('/order/summary', name: 'app_order_summary')] //, methods: ['POST'])]
+    public function add(Request $request, Cart $cart): Response
+    {
+        //permet de mettre en place une redirection contrairement à l'attribut @[Route]
+        if ($request->getMethod() != 'POST') {
+            return $this->redirectToRoute('app_cart');
+        }
+
+        $form = $this->createForm(OrderType::class, null, [
+            'addresses' => $this->getUser()->getAddresses(),
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $order = $form->getData();
+
+            //Stocker les infos en bdd
+
+        }
+        return $this->render('order/summary.html.twig', [
+            'choices' => $form->getData(),
+            'cart' => $cart->getCart(),
+            'totalWT' => $cart->getTotalWT(),
         ]);
     }
 }
